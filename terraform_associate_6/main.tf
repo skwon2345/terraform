ac75ebd1018a41fa072531c4d27e63ac2dbcb6df
sukkwono-hits <sukkwono@hits.ai>
2021-11-07 16:43:40 +0900

    terraform_associate_6: testing count meta-argument

---
+++ terraform_associate_6/main.tf
@@ -0,0 +1,27 @@
+# terraform confiuration blck
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "sukkwono"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "my_server" {
+  count = 2
+  ami           = "ami-0e5b6b6a9f3db6db8"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Server-${count.index}"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.my_server[*].public_ip
+}
