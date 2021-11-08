c88dd611dabd6e17f01668361602036c2e4d453a
sukkwono-hits <sukkwono@hits.ai>
2021-11-08 12:32:36 +0900

    terraform_associate_11: apply versioning
    - >= will get the latest version greater than the number on the right side
    - ex) >= 3.63.0 will get the latest version that is greater than or equal to 3.63.9
    - ex) ~> 1.0.0 get the latest version of rightmost number in 1.0.0 like 1.0.9

--- terraform_associate_11/main.tf
+++ terraform_associate_11/main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.63.0"
+      version = ">= 3.63.0"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
