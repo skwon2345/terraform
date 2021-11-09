fce7f2971db091c8fe708b1553a76b197d14669e
sukkwono-hits <sukkwono@hits.ai>
2021-11-09 11:24:39 +0900

    sktest_main: versioning and linting

--- sk_test/main.tf
+++ sk_test/main.tf
@@ -3,9 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.63.0"
+      version = ">= 3.63.0"
     }
   }
+
+  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
@@ -16,7 +18,7 @@ provider "aws" {
 
 resource "aws_iam_user" "hits_users" {
   for_each = var.users
-  name = each.key
+  name     = each.key
 
   tags = {
     access_level = each.value.access_level
@@ -25,7 +27,7 @@ resource "aws_iam_user" "hits_users" {
 
 resource "aws_iam_access_key" "hits_access_key" {
   for_each = aws_iam_user.hits_users
-  user  = each.value.name
+  user     = each.value.name
 }
 
 resource "aws_iam_group" "group" {
@@ -35,17 +37,17 @@ resource "aws_iam_group" "group" {
 resource "aws_iam_group_membership" "hits_iam_group" {
   name = "hits_iam_group"
 
-  users = [for user in aws_iam_user.hits_users: user.name]
+  users = [for user in aws_iam_user.hits_users : user.name]
   group = aws_iam_group.group.name
 }
 
 resource "aws_iam_user_login_profile" "u" {
   for_each = aws_iam_user.hits_users
-  user    = each.value.name
-  pgp_key = "keybase:sukkwono"
+  user     = each.value.name
+  pgp_key  = "keybase:sukkwono"
 }
 
 output "password" {
-  value = {for user_profile in aws_iam_user_login_profile.u: user_profile.user => user_profile.encrypted_password}
+  value     = { for user_profile in aws_iam_user_login_profile.u : user_profile.user => user_profile.encrypted_password }
   sensitive = true
 }
